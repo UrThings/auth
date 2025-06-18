b7b4bd1504a68dc5169b5cf597a477257453a39e
UrThings <tengis.tso@gmail.com>
2025-06-18 15:24:32 +0800

    some

--- src/app/some/page.tsx
+++ src/app/some/page.tsx
@@ -3,10 +3,29 @@
 import { useState } from "react";
 import ImageUploader from "../_components/ImageUploader";
 import ImagesUploader from "../_components/imagesUploader";
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const [profileImg, setProfileImg] = useState("");
   const [images, setImages] = useState<string[]>([]);
+  const [works, setWorks] = useState([{
+    startDate: "2017",
+    endDate: "Now",
+    company: "Senior Designer at Magic Design Co",
+    city: "Sn Francisco, CA",
+    img: ["https://i.pinimg.com/736x/46/b5/c7/46b5c7e944c2eb3cc3584d9bec415fa3.jpg","https://i.pinimg.com/736x/16/81/64/1681648ffb5f7fd4439c3788f0dcdd26.jpg" ],
+    url: "facebook.com"
+
+  },{
+    startDate: "2020",
+    endDate: "2024",
+    company: "Junior Designer at Magic Design Co",
+    city: "All Francisco, CA",
+    img: [],
+    url: "facebook.com"
+  }]);
+
+  const router = useRouter();
 
   return (
     <div>
@@ -46,6 +65,27 @@ export default function ProfilePage() {
           </div>
         )}
       </div>
+
+      {works.map((work) => (
+        <div className="flex">
+          <div className="flex">
+            <div>{work.startDate}</div>
+            -
+            <div>{work.endDate}</div>
+          </div>
+          <div>
+            <div onClick={() => {router.push(`/${work.url}`)}} >{work.company}</div>
+            <div>{work.city}</div>
+            <div className="flex gap-5">
+                {work.img?.map((i)=> (
+                    <div>
+                        <img className="w-auto h-[150px]" src={i} alt="" />
+                    </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
